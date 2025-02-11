81494f54f2defbf9cb859956b60cf2d0dd7e5740
Bruno Dantas <bmndantas@gmail.com>
2025-02-11 17:43:53 +0000

    IDriverPool added

---
+++ src/test/java/com/github/brunomndantas/flashscore/api/transversal/driverPool/DriverPoolExceptionTests.java
@@ -0,0 +1,36 @@
+package com.github.brunomndantas.flashscore.api.transversal.driverPool;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DriverPoolExceptionTests {
+
+    @Test
+    public void shouldHaveNoMessageAndNoCause() {
+        DriverPoolException exception = new DriverPoolException();
+
+        Assertions.assertNull(exception.getMessage());
+        Assertions.assertNull(exception.getCause());
+    }
+
+    @Test
+    public void shouldByPassMessage() {
+        String message = "Message";
+
+        DriverPoolException exception = new DriverPoolException(message);
+
+        Assertions.assertEquals(message, exception.getMessage());
+    }
+
+    @Test
+    public void shouldByMessageAndCause() {
+        String message = "Message";
+        Throwable cause = new Exception();
+
+        DriverPoolException exception = new DriverPoolException(message, cause);
+
+        Assertions.assertEquals(message, exception.getMessage());
+        Assertions.assertEquals(cause, exception.getCause());
+    }
+
+}
