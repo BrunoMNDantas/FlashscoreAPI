945a951c312ff4ea71692604616f5ddf8f6cb9cb
Bruno Dantas <bmndantas@gmail.com>
2025-02-13 22:59:16 +0000

    Defining routes hierarchically

--- src/main/java/com/github/brunomndantas/flashscore/api/serviceInterface/config/Routes.java
+++ src/main/java/com/github/brunomndantas/flashscore/api/serviceInterface/config/Routes.java
@@ -5,14 +5,14 @@ public class Routes {
     public static final String SPORTS_ROUTE = "/sports";
     public static final String SPORT_ROUTE = SPORTS_ROUTE + "/{sportId}";
 
-    public static final String REGIONS_ROUTE = "/regions";
-    public static final String REGION_ROUTE = REGIONS_ROUTE + "/{sportId}/{regionId}";
+    public static final String REGIONS_ROUTE = SPORT_ROUTE + "/regions";
+    public static final String REGION_ROUTE = REGIONS_ROUTE + "/{regionId}";
 
-    public static final String COMPETITIONS_ROUTE = "/competitions";
-    public static final String COMPETITION_ROUTE = COMPETITIONS_ROUTE + "/{sportId}/{regionId}/{competitionId}";
+    public static final String COMPETITIONS_ROUTE = REGION_ROUTE + "/competitions";
+    public static final String COMPETITION_ROUTE = COMPETITIONS_ROUTE + "/{competitionId}";
 
-    public static final String SEASONS_ROUTE = "/seasons";
-    public static final String SEASON_ROUTE = SEASONS_ROUTE + "/{sportId}/{regionId}/{competitionId}/{seasonId}";
+    public static final String SEASONS_ROUTE = COMPETITION_ROUTE + "/seasons";
+    public static final String SEASON_ROUTE = SEASONS_ROUTE + "/{seasonId}";
 
     public static final String MATCHES_ROUTE = "/matches";
     public static final String MATCH_ROUTE = MATCHES_ROUTE + "/{matchId}";
