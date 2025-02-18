354bd17e69e6799dbe265b7a212dfe17e06d7a2b
Bruno Dantas <bmndantas@gmail.com>
2025-02-18 12:45:06 +0000

    Test workflow

--- deploy/scripts/deploy_development.sh
+++ deploy/scripts/deploy_development.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 set -e
 
-echo "Deploying to the development environment..."
-sleep 2
-echo "Deployment to development completed!"
+echo "Triggering deployment for the Railway development environment..."
+
+RAILWAY_PROJECT_ID="5824a4b7-2bd9-45dd-96a5-188788ca1720"
+RAILWAY_ENVIRONMENT_ID="7d111a97-b938-4184-a351-a8237e553c98"
+RAILWAY_SERVICE_ID="cb31c136-511d-4fb7-a7da-5b0e278e1677"
+
+# GraphQL mutation to trigger deployment
+GRAPHQL_QUERY='{
+  "query": "mutation DeployService($projectId: String!, $environmentId: String!, $serviceId: String!) {
+    deploymentCreate(input: { projectId: $projectId, environmentId: $environmentId, serviceId: $serviceId }) { id }
+  }",
+  "variables": {
+    "projectId": "'"$RAILWAY_PROJECT_ID"'",
+    "environmentId": "'"$RAILWAY_ENVIRONMENT_ID"'",
+    "serviceId": "'"$RAILWAY_SERVICE_ID"'"
+  }
+}'
+
+# Trigger deployment
+curl -X POST "https://backboard.railway.app/graphql/v2" \
+     -H "Content-Type: application/json" \
+     -H "Authorization: Bearer $RAILWAY_API_TOKEN" \
+     -d "$GRAPHQL_QUERY"
+
+echo "Deployment request sent to Railway."
