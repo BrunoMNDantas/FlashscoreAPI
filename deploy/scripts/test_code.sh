47ea96746faaa843983b5d59b25878be5818e574
Bruno Dantas <bmndantas@gmail.com>
2025-02-18 12:17:36 +0000

    Test workflow

--- deploy/scripts/test_code.sh
+++ deploy/scripts/test_code.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "Running code tests..."
-sleep 2
-echo "Code tests completed!"
+echo "Running Gradle build and tests..."
+
+# Run Gradle build and tests from the project root
+./gradlew clean build test --tests "com.github.brunomndantas.flashscore.api.serviceInterface.controllers.*"
+
+echo "Gradle build and tests completed!"
