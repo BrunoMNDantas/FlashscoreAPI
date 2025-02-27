8c6be793930048b8f15d2dfd7b784b516a4c818b
Bruno Dantas <bmndantas@gmail.com>
2025-02-27 11:46:33 +0000

    Using Composite Actions and Reusable Workflows

--- deploy/scripts/notify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-     -d "chat_id=$CHAT_ID" \
-     -d "text=$MESSAGE" \
-     -d "parse_mode=Markdown"
--- deploy/scripts/test_code.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running Gradle build and tests..."
-
-# Run Gradle build and tests from the project root
-./gradlew clean build test
-
-echo "Gradle build and tests completed!"
