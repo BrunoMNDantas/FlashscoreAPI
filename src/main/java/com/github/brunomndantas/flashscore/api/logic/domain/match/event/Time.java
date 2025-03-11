93f7006c960c89ddb1f19acf5c379b12b883e783
Bruno Dantas <bmndantas@gmail.com>
2025-03-11 15:05:49 +0000

    Added Jakarta validations to Time domain entity

--- src/main/java/com/github/brunomndantas/flashscore/api/logic/domain/match/event/Time.java
+++ src/main/java/com/github/brunomndantas/flashscore/api/logic/domain/match/event/Time.java
@@ -1,6 +1,8 @@
 package com.github.brunomndantas.flashscore.api.logic.domain.match.event;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,12 +25,15 @@ public class Time {
 
 
     @Schema(description = "Period of the match in which the event occurred", example = "FIRST_HALF")
+    @NotNull
     private Period period;
 
     @Schema(description = "Minute in which the event occurred", example = "29")
+    @Min(0)
     private int minute;
 
     @Schema(description = "Extra minute in which the event occurred", example = "0")
+    @Min(0)
     private int extraMinute;
 
 }
